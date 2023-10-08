7318f25f0f0fb4817eef412c77dc9837d0e67b59
Rooman <Rooman@ROOMAN>
2023-10-09 00:20:46 +0330

    Add project files.

---
+++ Animal App/Animal.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal_App
+{
+    internal class Animal
+    {
+        public void Animal_Name()
+        {
+            Console.WriteLine("Animal List:");
+            Console.WriteLine("1- Cat");
+            Console.WriteLine("2- Cow");
+            Console.WriteLine("3- Parrot");
+            Console.WriteLine("4- Duck");
+            Console.WriteLine("5- Wolf");
+            Console.WriteLine("Please Select Animals Number:");
+        }
+        interface IAnimal
+        {
+            void Animal_Sound();
+            void Animal_Walk();
+            void Animal_Food();
+            void Animal_Features();
+            void Animal_Location();
+        }
+
+        public class Cat : IAnimal
+        {
+            public void Animal_Walk()
+            {
+                Console.WriteLine("Walk: on hands and feet");
+            }
+            public void Animal_Sound()
+            {
+                Console.WriteLine("Sound: Miuuuu Miuuuu");
+            } 
+            public void Animal_Food()
+            {
+                Console.WriteLine("Food: Meat - Milk");
+            }
+            public void Animal_Features()
+            {
+                Console.WriteLine("Features: Mouse Hunter - Pet - Mammal");
+            }
+            public void Animal_Location()
+            {
+                Console.WriteLine("Location: Home - Street");
+            }
+        }
+        public class Cow : IAnimal
+        {
+            public void Animal_Walk()
+            {
+                Console.WriteLine("Walk: on hands and feet");
+            }
+            public void Animal_Sound()
+            {
+                Console.WriteLine("Sound: Mooo Moooo");
+            }
+            public void Animal_Food()
+            {
+                Console.WriteLine("Food: Plant");
+            }
+            public void Animal_Features()
+            {
+                Console.WriteLine("Features: giving milk - Pet - Mammal");
+            }
+            public void Animal_Location()
+            {
+                Console.WriteLine("Location: Farm");
+            }
+        }
+        public class Parrot : IAnimal
+        {
+            public void Animal_Walk()
+            {
+                Console.WriteLine("Walk: on feet");
+            }
+            public void Animal_Sound()
+            {
+                Console.WriteLine("Sound: Imitation of human voice");
+            }
+            public void Animal_Food()
+            {
+                Console.WriteLine("Food: seed");
+            }
+            public void Animal_Features()
+            {
+                Console.WriteLine("Features: Imitation of human voice - Pet - laying eggs - Flight");
+            }
+            public void Animal_Location()
+            {
+                Console.WriteLine("Location: Home And Jungle");
+            }
+        }
+        public class Duck : IAnimal
+        {
+            public void Animal_Walk()
+            {
+                Console.WriteLine("Walk: on feet");
+            }
+            public void Animal_Sound()
+            {
+                Console.WriteLine("Sound: ghoooak");
+            }
+            public void Animal_Food()
+            {
+                Console.WriteLine("Food: seed - Plant");
+            }
+            public void Animal_Features()
+            {
+                Console.WriteLine("Features: the swimmer - Pet - laying eggs");
+            }
+            public void Animal_Location()
+            {
+                Console.WriteLine("Location: Farme - Lake ");
+            }
+        }
+        public class Wolf : IAnimal
+        {
+            public void Animal_Walk()
+            {
+                Console.WriteLine("Walk: on hands and feet");
+            }
+            public void Animal_Sound()
+            {
+                Console.WriteLine("Sound: aaaaoooooo aaaoooooooo");
+            }
+            public void Animal_Food()
+            {
+                Console.WriteLine("Food: Meat");
+            }
+            public void Animal_Features()
+            {
+                Console.WriteLine("Features: Hunter - wild animal - Mammal");
+            }
+            public void Animal_Location()
+            {
+                Console.WriteLine("Location: Forest - Mountain - Jungle");
+            }
+        }
+    }
+}
---
+++ Animal App/Program.cs
@@ -0,0 +1,64 @@
+﻿using Animal_App;
+using System.Data.Common;
+
+Animal animal = new Animal();
+Animal.Cat cat = new Animal.Cat();
+Animal.Cow cow = new Animal.Cow();
+Animal.Duck duck = new Animal.Duck();
+Animal.Wolf wolf = new Animal.Wolf();
+Animal.Parrot parrot = new Animal.Parrot();
+Console.WriteLine("Welcome");
+Console.WriteLine("*****************************************************************");
+Console.WriteLine("App Information: Animal Calss And Object");
+Console.WriteLine("*****************************************************************");
+animal.Animal_Name();
+int Animal = int.Parse(Console.ReadLine());
+Console.WriteLine("*****************************************************************");
+if (Animal == 1)
+{
+    cat.Animal_Walk();
+    cat.Animal_Sound();
+    cat.Animal_Food();
+    cat.Animal_Features();
+    cat.Animal_Location();
+}
+else if (Animal == 2)
+{
+    cow.Animal_Walk();
+    cow.Animal_Sound();
+    cow.Animal_Food();
+    cow.Animal_Features();
+    cow .Animal_Location();
+}
+else if (Animal == 3)
+{
+    parrot.Animal_Walk();
+    parrot.Animal_Sound();
+    parrot.Animal_Food();
+    parrot.Animal_Features();
+    parrot.Animal_Location();
+}
+else if (Animal == 4)
+{
+    duck.Animal_Walk();
+    duck.Animal_Sound();
+    duck.Animal_Food();
+    duck.Animal_Features();
+    duck.Animal_Location();
+}
+else if (Animal == 5)
+{
+    wolf.Animal_Walk();
+    wolf.Animal_Sound();
+    wolf.Animal_Food();
+    wolf.Animal_Features();
+    wolf.Animal_Location();
+}
+else
+{
+    Console.WriteLine("Error");
+    return;
+}
+Console.WriteLine("*****************************************************************");
+Console.ReadKey();
+
